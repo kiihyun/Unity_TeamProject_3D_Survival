d0c6c96591f3a5ca2559109626f2d1324380e25c
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-05-27 14:33:31 +0900

    feat : 아이템 오브젝트 추가
    아이템 오브젝트와 스크립트 오브젝트를 추가하였습니다.
    우선 예시로 만든 프리팹을 통해 어떤 식으로 이용 가능한지 알 수 있습니다.

---
+++ Assets/SOs/Recipes/CraftItemData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CraftRecipe", menuName = "Crafting/Recipe")]
+public class CraftItemData : ScriptableObject
+{
+    [Header("Output")]
+    public ItemData resultItem;      // 만들어지는 아이템
+    public int resultAmount = 1;     // 만들어지는 수량
+
+    [Header("Required Materials")]
+    public MaterialRequirement[] materials;
+
+    [Header("Craft Conditions")]
+    //여기 부분은 다양하게 설정 가능(주위에 강이 있어야 한다, 선행 조건이 있어야 한다 등등)
+    public bool requiresNearFire = false;
+
+}
+
+[System.Serializable]
+public class MaterialRequirement
+{
+    public ItemData item;    // 필요한 재료
+    public int amount;       // 재료 수량
+}
