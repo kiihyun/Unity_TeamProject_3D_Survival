39063af12e6b71cedcbea1063d2fd99932abde51
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-05-27 14:40:43 +0900

    refactor 크래프트 아이템 코드 설명 추가

--- Assets/SOs/Recipes/CraftItemData.cs
+++ Assets/SOs/Recipes/CraftItemData.cs
@@ -11,8 +11,9 @@ public class CraftItemData : ScriptableObject
     public MaterialRequirement[] materials;
 
     [Header("Craft Conditions")]
-    //여기 부분은 다양하게 설정 가능(주위에 강이 있어야 한다, 선행 조건이 있어야 한다 등등)
+    //여기 부분은 다양하게 설정 가능(주위에 불이 있어야 한다, 선행 조건이 있어야 한다 등등)
     public bool requiresNearFire = false;
+    public string requiredCondition; // 예: 퀘스트를 완료해야 만들 수 있다.라고 조건을 만들고 싶다면 "FinishedQuest1"를 확인하도록
 
 }
 
